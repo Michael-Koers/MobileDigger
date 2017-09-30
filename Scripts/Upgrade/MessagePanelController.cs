26924c4c1b9d23a2e048d0238aa8a32281557608
Michael Koers <michaelkoers95@gmail.com>
2017-09-30 23:24:49 +0200

    Added some functionality to the blacksmith icon

---
+++ Scripts/Upgrade/MessagePanelController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagePanelController : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GetComponentInParent<CanvasGroup>().blocksRaycasts = false;
+        GetComponentInParent<CanvasGroup>().alpha = 0f;
+
+    }
+}
